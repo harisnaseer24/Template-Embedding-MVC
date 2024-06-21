1b3e11cea9c39f1b601b12a771f303f13bcf2d9e
harisnaseer24 <harisnaseer258@gmail.com>
2024-06-21 12:49:02 +0500

    Client Ecommerce template added.

---
+++ 2301C2TempEmbedding/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using _2301C2TempEmbedding.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _2301C2TempEmbedding.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        } 
+        public IActionResult About()
+        {
+            return View();
+        }  
+        public IActionResult Products()
+        {
+            return View();
+        }
+
+       
+    }
+}
