c5acd5438de6eaa5b21c4494c7648509fc57d3c1
harisnaseer24 <harisnaseer258@gmail.com>
2024-06-24 13:03:42 +0500

    Admin template added and model added with validation

---
+++ 2301C2TempEmbedding/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _2301C2TempEmbedding.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
