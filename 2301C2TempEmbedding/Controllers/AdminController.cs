47c73b02470de8de601b4ec822f2a481ab0acc20
harisnaseer24 <harisnaseer258@gmail.com>
2024-06-28 13:09:50 +0500

    role based authentication using sessions

--- 2301C2TempEmbedding/Controllers/AdminController.cs
+++ 2301C2TempEmbedding/Controllers/AdminController.cs
@@ -1,13 +1,23 @@
 ﻿using _2301C2TempEmbedding.Models;
+
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace _2301C2TempEmbedding.Controllers
 {
     public class AdminController : Controller
     {
+        
         public IActionResult Index()
         {
-            return View();
+            if (HttpContext.Session.GetString("role") == "admin") {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+           
         }
         public IActionResult AddProduct()
         {
@@ -32,6 +42,38 @@ namespace _2301C2TempEmbedding.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+       
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(string email, string pass)
+        {
+            if(email=="admin@gmail.com" && pass == "123")
+            {
+                HttpContext.Session.SetString("adminEmail", email);
+                HttpContext.Session.SetString("role", "admin");
+
+
+
+                // return RedirectToAction("Index","Home");
+                return RedirectToAction("Index");
+            }
+            else if (email == "user@gmail.com" && pass == "123")
+            {
+                HttpContext.Session.SetString("userEmail", email);
+                HttpContext.Session.SetString("role", "user");
+
+                return RedirectToAction("Index","Home");
+
+            }
+            else
+            {
+
+                ViewBag.msg = "Invalid Credentials";
+                return View();
+            }
+           
         }
     }
 }
--- 2301C2TempEmbedding/Program.cs
+++ 2301C2TempEmbedding/Program.cs
@@ -3,6 +3,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//session configuration
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(60);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +29,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseSession();
 
 app.UseAuthorization();
 
