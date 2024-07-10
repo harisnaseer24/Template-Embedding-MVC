6c40fe3644ec77bd4341debc087a6e3216ad477d
harisnaseer24 <harisnaseer258@gmail.com>
2024-07-10 12:53:49 +0500

    edit and delete operations completed

--- 2301C2TempEmbedding/Controllers/ProductController.cs
+++ 2301C2TempEmbedding/Controllers/ProductController.cs
@@ -33,5 +33,42 @@ namespace _2301C2TempEmbedding.Controllers
             return View();
             }
         }
+
+        public IActionResult Edit(int id)
+        {
+            var product= db.Products.Find(id);
+            return View(product);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Product prd)
+        {
+
+            if (ModelState.IsValid)
+            {
+                db.Products.Update(prd);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+
+            return View();
+            }
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = db.Products.Find(id);
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult Delete(Product prd)
+        {
+            db.Products.Remove(prd);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
